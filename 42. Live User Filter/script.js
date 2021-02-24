23f7e853c9c9a6e17a3e7532ffd24bc6a5143de4
dLeczycki <danielleczycki@gmail.com>
2021-02-24 19:33:53 +0100

    Day 42 Finished!

---
+++ 42. Live User Filter/script.js
@@ -0,0 +1,35 @@
+const searchInput = document.querySelector('div.filter-box input');
+const usersList = document.querySelector('ul.users-list');
+
+const RANDOMUSER_API = 'https://randomuser.me/api/?results=40';
+
+searchInput.addEventListener('input', e => search(e.target.value.toLowerCase()));
+
+fetchUsers();
+
+function search(filter = '') {
+    const users = [...usersList.querySelectorAll('li')];
+    console.log(users);
+    const filteredUsers = users.filter(user => user.innerText.toLowerCase().includes(filter));
+
+    users.forEach(user => user.classList.add('hide'));
+    filteredUsers.forEach(user => user.classList.remove('hide'));
+}
+
+async function fetchUsers() {
+    await fetch(RANDOMUSER_API).then(res => res.json()).then(result => {
+        result.results.forEach(user => {
+            const userBox = document.createElement('li');
+            userBox.classList.add('user-box');
+            userBox.innerHTML =
+                `
+            <img src='${user.picture.thumbnail}'>
+            <div>
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+            `
+            usersList.appendChild(userBox);
+        })
+    });
+}
